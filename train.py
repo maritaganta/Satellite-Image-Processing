7cbf3d5675b367740a2464af5c134a3dbeb8fa81
Marita Georganta <maritaganta@gmail.com>
2022-06-07 17:05:57 +0200

    training, metrics, history

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
-import os
 import pandas as pd
+from generator import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from models import *
+from plot_history import plot_history
 
-#  TODO: Plot history?
-
+PLOT_HISTORY = True
 MASK_ALGORITHM = 'Kumar-Roy'
 
 N_FILTERS = 64
@@ -21,6 +24,8 @@ IMAGES_DATAFRAME = '../../FireDetectionProcesses/dataset/images_masks.csv'
 OUTPUT_DIR = '../../FireDetectionProcesses/dataset/train_output'
 
 WORKERS = 4
+
+EARLY_STOP_PATIENCE = 5
 
 CHECKPOINT_PERIOD = 5
 CHECKPOINT_MODEL_NAME = 'checkpoint-{}-{}-epoch_{{epoch:02d}}.hdf5'.format(MODEL_NAME, MASK_ALGORITHM)
@@ -59,3 +64,39 @@ masks_validation = [os.path.join(MASKS_PATH, mask) for mask in y_val['masks']]
 
 train_generator = generator_from_lists(images_train, masks_train, batch_size=BATCH_SIZE)
 validation_generator = generator_from_lists(images_validation, masks_validation, batch_size=BATCH_SIZE)
+
+model = get_model(MODEL_NAME, input_height=IMAGE_SIZE[0], input_width=IMAGE_SIZE[1],
+                  n_filters=N_FILTERS, n_channels=N_CHANNELS)
+
+model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=EARLY_STOP_PATIENCE)
+checkpoint = ModelCheckpoint(os.path.join(OUTPUT_DIR, CHECKPOINT_MODEL_NAME),
+                             monitor='loss', verbose=1, save_best_only=True, mode='auto', period=CHECKPOINT_PERIOD)
+
+if INITIAL_EPOCH > 0:
+    model.load_weights(RESTART_FROM_CHECKPOINT)
+
+print('Training using {}...'.format(MASK_ALGORITHM))
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=len(images_train) // BATCH_SIZE,
+    validation_data=validation_generator,
+    validation_steps=len(images_validation) // BATCH_SIZE,
+    callbacks=[checkpoint, es],
+    epochs=EPOCHS,
+    workers=WORKERS,
+    initial_epoch=INITIAL_EPOCH
+)
+print('Train finished!')
+
+
+print('Saving weights')
+model_weights_output = os.path.join(OUTPUT_DIR, FINAL_WEIGHTS_OUTPUT)
+model.save_weights(model_weights_output)
+print("Weights Saved: {}".format(model_weights_output))
+
+if PLOT_HISTORY:
+    plot_history(history, OUTPUT_DIR)
